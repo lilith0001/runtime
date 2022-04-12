06f70a63715dc12e3aa4319fc303c91e67ba420a
lilith <ninidev@protonmail.com>
2022-04-12 14:40:17 -0300

    added averaging, fixed some C code

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/time.h>
+#include <time.h>
 
 int fib(int n)
 {
--- sum/sum.c
+++ sum/sum.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/time.h>
+#include <time.h>
 int main(int argc, char** argv)
 {
 	srand(time(NULL));
